97b66480d371ac599c5eede1b781e73c175afef0
Francisco Farfan <francisco.farfan.lazo@gmail.com>
2021-03-07 20:24:55 -0500

    Add aan example of validating using facts

--- index.js
+++ index.js
@@ -3,4 +3,32 @@ const app = fastify();
 const { Engine } = require("json-rules-engine");
 let engine = new Engine();
 
+engine.addRule({
+  conditions: {
+    all: [
+      {
+        fact: "temperature",
+        operator: "equal",
+        value: 100
+      }
+    ]
+  },
+  onSuccess() {
+    console.log("On success called")
+  },
+  onFailure() {
+    console.log("On failure called")
+  },
+  event: {
+    type: "messge",
+    params: {
+      data: "hello-world!"
+    }
+  }
+});
+
+const facts = { temperature: 200};
+
+engine.run(facts);
+
 app.listen(3000);
